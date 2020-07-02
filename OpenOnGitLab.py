029c5523db7268d7727fa691d786c5ecbd8faefc
Роман Бадмаев <barv@inbox.ru>
2020-07-02 13:30:45 +0300

    Add OpenOnGitLab command

---
+++ OpenOnGitLab.py
@@ -0,0 +1,49 @@
+
+import sublime, sublime_plugin
+import os, glob, subprocess
+import webbrowser
+import re
+
+class OpenOnGitlabCommand(sublime_plugin.WindowCommand):
+
+    def run(self, params = []):
+        url = self.getLink()
+        print (url)
+        webbrowser.open(url)
+
+    def getRelativePath(self):
+        folder, path = os.path.split(self.window.active_view().file_name())
+        def isAGitRoot(folder):
+            return os.path.exists(os.path.join(folder, ".git"))
+
+        while not isAGitRoot(folder):
+            next_folder, sub = os.path.split(folder)
+            if folder == next_folder:
+                return None
+
+            folder = next_folder
+            path = sub + '/' + path
+
+        return path
+
+    def getLink(self):
+        p = subprocess.Popen(["git", "config", "--get", "remote.origin.url"],
+            stdout=subprocess.PIPE,
+            cwd=os.path.dirname(self.window.active_view().file_name()))
+        url, err = p.communicate()
+        url = url.decode().strip()
+        url = re.sub('[^/@]*@', '', url)
+
+        if len(url) > 4 and url[-4:] == ".git":
+            url = url[:-4]
+
+        p = subprocess.Popen(["git", "rev-parse", "HEAD"],
+            stdout=subprocess.PIPE,
+            cwd=os.path.dirname(self.window.active_view().file_name()))
+        branch, err = p.communicate()
+        branch = branch.decode().strip()
+
+        row, col = self.window.active_view().rowcol(self.window.active_view().sel()[0].begin())
+
+        return url + '/blob/' + branch + '/' + self.getRelativePath() + '#L' + str(row + 1)
+
